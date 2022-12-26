8a29c65ae250a3757d7154672eeaf53e3042175d
jobas <jobas@gmu.edu>
2022-12-25 21:00:23 -0500

    adding build

---
+++ client/build/precache-manifest.3f1fd8abbb533210656b417bbf1db523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec60861fb1baf888fac",
+    "url": "/static/css/main.0085f6a9.chunk.css"
+  },
+  {
+    "revision": "7ec60861fb1baf888fac",
+    "url": "/static/js/main.7ec60861.chunk.js"
+  },
+  {
+    "revision": "0cbf7247ea31387e4dd0",
+    "url": "/static/js/1.0cbf7247.chunk.js"
+  },
+  {
+    "revision": "4b54de0fb1cf5bafe2e2",
+    "url": "/static/css/2.79f02ba6.chunk.css"
+  },
+  {
+    "revision": "4b54de0fb1cf5bafe2e2",
+    "url": "/static/js/2.4b54de0f.chunk.js"
+  },
+  {
+    "revision": "bc46a21ab4a52c3e1ef8",
+    "url": "/static/js/runtime~main.bc46a21a.js"
+  },
+  {
+    "revision": "517d50e3ea4b4b52143c8011fb918e78",
+    "url": "/index.html"
+  }
+];
