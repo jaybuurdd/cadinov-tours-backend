c0979ee2ba00626cfa5e37b2952e9edbbb415137
jobas <jobas@gmu.edu>
2022-12-29 22:58:36 -0500

    temp allowing api request from anywhere

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,18 +1,12 @@
-const allowedOrigins = [
-    'https://cadinov.onrender.com',
-  ];
-  
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-  };
+const allowedOrigins = ['*'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    callback(null, true);
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
   
   module.exports = corsOptions;
   
